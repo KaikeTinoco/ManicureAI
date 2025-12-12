b14a6180d6a7cbd1676e491d2433c568be5c365a
Kaike <kaiketinocon@outlook.com>
2025-12-12 01:08:58 -0300

    first commit

---
+++ src/test/java/com/manicure/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manicure.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
