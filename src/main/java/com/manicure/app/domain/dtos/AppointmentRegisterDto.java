d3f97ae394e10923246a798b258ec0af626e7bf9
Kaike <kaiketinocon@outlook.com>
2025-12-18 20:17:30 -0300

    tweaks in the appoitment's and client's register dtos

--- src/main/java/com/manicure/app/domain/dtos/AppointmentRegisterDto.java
+++ src/main/java/com/manicure/app/domain/dtos/AppointmentRegisterDto.java
@@ -1,14 +1,18 @@
 package com.manicure.app.domain.dtos;
 
 import com.manicure.app.domain.enuns.Services;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AppointmentRegisterDto {
     private String clientName;
 
--- src/main/java/com/manicure/app/domain/dtos/ClientRegisterDto.java
+++ src/main/java/com/manicure/app/domain/dtos/ClientRegisterDto.java
@@ -1,10 +1,14 @@
 package com.manicure.app.domain.dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ClientRegisterDto {
     private String whatsappId;
     private String clientName;
